0e31d64dce6dba54db760f04a6f93a550cc938fc
Guilherme Farias <109701085+GuiFxFarias@users.noreply.github.com>
2025-08-02 08:58:38 -0300

    apagando tudo do bettersqlite

--- vectors/vectorDB.js
+++ vectors/vectorDB.js
@@ -1,82 +1,85 @@
-// const Database = require('better-sqlite3');
-// const path = require('path');
+const conexao = require('../conexao');
 
-// class VectorDB {
-//   constructor() {
-//     this.dbPath = path.join(__dirname, '../database/vetores.db');
-//     this.db = new Database(this.dbPath);
+class VectorDB {
+  executaQuery(sql, parametros) {
+    return new Promise((res, rej) => {
+      conexao.query(sql, parametros, (error, results) => {
+        if (error) {
+          console.log('Erro na query: ' + error);
+          return rej(error);
+        }
+        return res(results);
+      });
+    });
+  }
 
-//     this.db
-//       .prepare(
-//         `CREATE TABLE IF NOT EXISTS documentos (
-//           id INTEGER PRIMARY KEY AUTOINCREMENT,
-//           texto TEXT,
-//           embedding TEXT
-//         )`
-//       )
-//       .run();
-//   }
+  async criarTabela() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS documentos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        texto TEXT,
+        embedding TEXT
+      )
+    `;
+    await this.executaQuery(sql);
+  }
 
-//   limparTodosOsVetores() {
-//     const stmt = this.db.prepare(`DELETE FROM documentos`);
-//     stmt.run();
-//     return true;
-//   }
+  limparTodosOsVetores() {
+    const sql = `DELETE FROM documentos`;
+    return this.executaQuery(sql);
+  }
 
-//   inserirDocumento(texto, embedding) {
-//     const embeddingStr = JSON.stringify(embedding);
-//     const stmt = this.db.prepare(
-//       `INSERT INTO documentos (texto, embedding) VALUES (?, ?)`
-//     );
-//     const info = stmt.run(texto, embeddingStr);
-//     return info.lastInsertRowid;
-//   }
+  inserirDocumento(texto, embedding) {
+    const embeddingStr = JSON.stringify(embedding);
+    const sql = `INSERT INTO documentos (texto, embedding) VALUES (?, ?)`;
+    return this.executaQuery(sql, [texto, embeddingStr]);
+  }
 
-//   buscarTodos() {
-//     const stmt = this.db.prepare(`SELECT * FROM documentos`);
-//     return stmt.all();
-//   }
+  buscarTodos() {
+    const sql = `SELECT * FROM documentos`;
+    return this.executaQuery(sql);
+  }
 
-//   buscarMaisSimilar(embeddingConsulta) {
-//     const documentos = this.buscarTodos();
+  async buscarMaisSimilar(embeddingConsulta) {
+    const documentos = await this.buscarTodos();
 
-//     const calcularSimilaridade = (a, b) => {
-//       if (!Array.isArray(a) || !Array.isArray(b) || a.length !== b.length) {
-//         return -1; // Vetores incompatíveis
-//       }
+    const calcularSimilaridade = (a, b) => {
+      if (!Array.isArray(a) || !Array.isArray(b) || a.length !== b.length) {
+        return -1;
+      }
 
-//       const dotProduct = a.reduce((sum, val, i) => sum + val * b[i], 0);
-//       const magA = Math.sqrt(a.reduce((sum, val) => sum + val * val, 0));
-//       const magB = Math.sqrt(b.reduce((sum, val) => sum + val * val, 0));
+      const dotProduct = a.reduce((sum, val, i) => sum + val * b[i], 0);
+      const magA = Math.sqrt(a.reduce((sum, val) => sum + val * val, 0));
+      const magB = Math.sqrt(b.reduce((sum, val) => sum + val * val, 0));
 
-//       return magA === 0 || magB === 0 ? -1 : dotProduct / (magA * magB);
-//     };
+      return magA === 0 || magB === 0 ? -1 : dotProduct / (magA * magB);
+    };
 
-//     let maisSimilar = null;
-//     let maiorSimilaridade = -1;
+    let maisSimilar = null;
+    let maiorSimilaridade = -1;
 
-//     for (const doc of documentos) {
-//       let vetor;
-//       try {
-//         vetor = JSON.parse(doc.embedding);
-//       } catch (err) {
-//         console.warn(
-//           `❌ Erro ao parsear embedding do documento ID ${doc.id}:`,
-//           err
-//         );
-//         continue;
-//       }
+    for (const doc of documentos) {
+      let vetor;
+      try {
+        vetor = JSON.parse(doc.embedding);
+      } catch (err) {
+        console.warn(
+          `❌ Erro ao parsear embedding do documento ID ${doc.id}:`,
+          err
+        );
+        continue;
+      }
 
-//       const similaridade = calcularSimilaridade(vetor, embeddingConsulta);
+      const similaridade = calcularSimilaridade(vetor, embeddingConsulta);
 
-//       if (similaridade > maiorSimilaridade) {
-//         maiorSimilaridade = similaridade;
-//         maisSimilar = { ...doc, similaridade };
-//       }
-//     }
+      if (similaridade > maiorSimilaridade) {
+        maiorSimilaridade = similaridade;
+        maisSimilar = { ...doc, similaridade };
+      }
+    }
 
-//     return maisSimilar;
-//   }
-// }
+    return maisSimilar;
+  }
+}
 
-// module.exports = new VectorDB();
+module.exports = new VectorDB();
